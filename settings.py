3309fc48deb61f738f1d21111686405fab43ceaf
Konner Finney <konner.business@gmail.com>
2021-10-14 16:23:40 -0700

    Adding required classes + bug fixes
    added new classes for class objects and settings classes. Added RegEx parsing into gsearch.py to confirm quizlet was the website being scraped.

---
+++ settings.py
@@ -0,0 +1,13 @@
+import json
+from school_class import dump_info
+# will create or edit local json file for local machine settings
+
+# settings overview:
+# stores classes in json elements 
+
+def class_to_json(self,list_of_classes: list) -> None:
+    for c in list_of_classes:
+        # store class data in dict
+        # then add dict into json dict with key = name-section : value = all class data
+        pass
+
