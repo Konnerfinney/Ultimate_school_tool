b421f498465dd7f5b1851e8e0f23d118098646b6
Konner Finney <konner.business@gmail.com>
2021-10-16 14:26:40 -0700

    added path to current directory
    Added path to current directory to create json settings file

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import json
-from school_class import dump_info
+import os
 # will create or edit local json file for local machine settings
 
+settings_dict = dict()
 # settings overview:
 # stores classes in json elements 
 
@@ -9,5 +10,19 @@ def class_to_json(self,list_of_classes: list) -> None:
     for c in list_of_classes:
         # store class data in dict
         # then add dict into json dict with key = name-section : value = all class data
-        pass
+        temp_list = c.dump_info()
+        settings_dict[temp_list[0]] = temp_list[1]
+
+def create_settings_file():
+    cwd = os.getcwd()
+    if not os.path.exists(cwd + "/settings.json"):
+        open("settings.json","x")
+
+def update_settings():
+    pass
+
+def get_settings() -> dict:
+    pass
+
+
 
